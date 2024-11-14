d9239d4313d98c52e09cb31c091d98e57404045b
hariharans <x23201550@student.ncirl.ie>
2024-11-14 20:00:45 +0000

    adding minor changes

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ import installNpmPackage from './utils/cache.js';
 import executeContainer from './utils/execution.js';
 import readCSVFile from './utils/readCSVfile.js';
 import writeCSVFile from './utils/writeCSVfile.js';
+import executeOpenwhisk from './utils/executeOpenwhisk.js';
 
+/**
+ * 
+ * Error handling pending
+ * code cleanse
+ */
 
 const args = process.argv.slice(2);
-const argCount = args.length;
+
 
 
 class Main {
@@ -17,6 +23,15 @@ class Main {
     async sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
+    /**
+ * Initiates the process by checking for a warm container, installing necessary npm packages if the container is cold,
+ * executing the container, and logging the execution details to a CSV file.
+ *
+ * @async
+ * @function initiator
+ * @param {Object} this.args - The arguments to be passed to the container.
+ * @returns {Promise<void>} - A promise that resolves when the process is complete.
+ */
     async initiator() {
         try {
                 const { executionType, containerName } = await checkWarmContainer('node');
@@ -38,6 +53,16 @@ class Main {
             process.exit(1);
         }
     }
+/**
+ * Orchestrates the process by reading data from a CSV file, checking for a warm container,
+ * installing necessary npm packages if the container is cold, and executing the container.
+ * The process is repeated for a specified number of times with delays between each execution.
+ *
+ * @async
+ * @function orchestrator
+ * @param {Object} this.args - The arguments to be passed to the container.
+ * @returns {Promise<void>} - A promise that resolves when the orchestration process is complete.
+ */
     async orchestrator() {
         try {
             const data = await readCSVFile('/home/hari73118/project/dataset/gru_wait_delay.csv');
@@ -65,14 +90,40 @@ class Main {
             process.exit(1);
         }
     }
+/**
+ * Executes an OpenWhisk action with the provided arguments and logs any errors that occur.
+ *
+ * @async
+ * @function openWhisk
+ * @param {Object} this.args - The arguments to be passed to the OpenWhisk action.
+ * @returns {Promise<void>} - A promise that resolves when the OpenWhisk action execution is complete.
+ */
+    async openWhisk() {
+        try {
+            const executionTime = await executeOpenwhisk(this.args);
+        } catch (error) {
+            console.error('Error in openWhisk:', error);
+            process.exit(1);
+        }
+    }
 }
 
 
 const main = new Main(args);
-//orchestrate
-if (args[1] === 'orchestrate') main.orchestrator();
 
-//initiate
-if (args[1] === 'initiate') main.initiator();
+switch (args[1]) {
+    case 'orchestrate':
+      main.orchestrator();
+      break;
+    case 'initiate':
+      main.initiator();
+      break;
+    case 'openwhisk':
+      main.openWhisk();
+      break;
+    default:
+      console.log('Invalid argument');
+      break;
+}
 
 
---
+++ utils/executeOpenwhisk.js
@@ -0,0 +1,35 @@
+
+import { exec } from 'child_process';
+import util from 'util';
+import { performance } from 'perf_hooks';
+
+
+/**
+ * handle in use conatiners
+ */
+
+const execPromise = util.promisify(exec);
+
+const executeOpenwhisk = async (args) => {
+        try {
+            console.log('Creating Openwhisk Action...');
+            const startTime = performance.now();
+            const {stdout: createStdout} = await execPromise(`sudo wsk -i action create simpleAction --kind nodejs:default /home/hari73118/project/${args[0]} `);
+            console.log('Invoking Openwhisk Action...');
+            const {stdout: invokeStdout} = await execPromise(`sudo wsk -i action invoke simpleAction --blocking`);
+            const endTime = performance.now();
+            console.log('Deleting Openwhisk Action...');
+            const {stdout: deleteStdout} = await execPromise(`sudo wsk -i action delete simpleAction`);
+            console.log('Total Execution time is',endTime - startTime);
+            return endTime - startTime;
+
+
+        } catch (error) {   
+            console.error(error);
+            process.exit(1);
+        }
+
+}
+
+export default executeOpenwhisk;
+
